4118a37aa64c16b3340f6bf2a196d702dea7402f
you21979 <you2197901@gmail.com>
2015-01-21 16:01:07 +0900

    mod

--- agent_loader.js
+++ agent_loader.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var scan = exports.scan = function(dir){
     var files = fs.readdirSync(dir);
-    return files.filter(function(v){return !fs.statSync([dir, v].join('/')).isDirectory()}).
+    return files.filter(function(v){return v.match(/.json/) && !fs.statSync([dir, v].join('/')).isDirectory()}).
         map(function(v){
             return {
                 name : v,
